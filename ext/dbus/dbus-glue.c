35c1034465fad35816db3a74b6b7f9e02eb4ff73
acornet <acornet@a7605b1d-0065-4901-a58a-044c66c5aa93>
2007-02-22 15:23:59 +0000

    New messages constructor for signals, method return and errors.
    git-svn-id: https://svn.luon.net/svn/ruby-dbus/trunk@9 a7605b1d-0065-4901-a58a-044c66c5aa93

--- ext/dbus/dbus-glue.c
+++ ext/dbus/dbus-glue.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2007 Arnaud Cornet.
  *
- * TODO: chose and display license ;)
+ * TODO: Add GPL legal header.
  *
  */
 
@@ -88,9 +88,67 @@ static VALUE rubydbus_message_new(VALUE class, VALUE msg_type)
 	VALUE ret;
 
 	message = dbus_message_new(NUM2INT(msg_type));
-
 	ret = Data_Wrap_Struct(cDBUSMessage, 0,
 			dbus_message_unref, message);
+	rb_obj_call_init(ret, 0, 0);
+	return ret;
+}
+
+VALUE rubydbus_message_new_method_call(VALUE class, VALUE rdestination,
+		VALUE rpath, VALUE rinterface, VALUE rmethod)
+{
+	DBusMessage *message;
+	VALUE ret;
+
+	message = dbus_message_new_method_call(StringValuePtr(rdestination),
+			StringValuePtr(rpath), StringValuePtr(rinterface),
+			StringValuePtr(rmethod));
+
+	ret = Data_Wrap_Struct(cDBUSMessage, 0, dbus_message_unref, message);
+	rb_obj_call_init(ret, 0, 0);
+	return ret;
+}
+
+VALUE rubydbus_message_new_method_return(VALUE self)
+{
+	DBusMessage *message, *method_return;
+	VALUE ret;
+
+	Data_Get_Struct(self, DBusMessage, message);
+
+	method_return = dbus_message_new_method_return(message);
+	ret = Data_Wrap_Struct(cDBUSMessage, 0, dbus_message_unref,
+			method_return);
+	rb_obj_call_init(ret, 0, 0);
+	return ret;
+}
+
+VALUE rubydbus_message_new_signal(VALUE class, VALUE rpath, VALUE rinterface,
+		VALUE rname)
+{
+	DBusMessage *message;
+	VALUE ret;
+
+	message = dbus_message_new_signal(StringValuePtr(rpath),
+			StringValuePtr(rinterface), StringValuePtr(rname));
+	ret = Data_Wrap_Struct(cDBUSMessage, 0, dbus_message_unref, message);
+	rb_obj_call_init(ret, 0, 0);
+	return ret;
+}
+
+VALUE rubydbus_message_new_error(VALUE self, VALUE rerror_name,
+		VALUE rerror_message)
+{
+	DBusMessage *message;
+	DBusMessage *error;
+
+	Data_Get_Struct(self, DBusMessage, message);
+
+	DBusMessage *error = dbus_message_new_error(DBusMessage *message,
+			StringValuePtr(rerror_name),
+			StringValuePtr(rerror_message));
+	ret = Data_Wrap_Struct(cDBUSMessage, 0, dbus_message_unref, error);
+	rb_obj_call_init(ret, 0, 0);
 	return ret;
 }
 
@@ -130,7 +188,16 @@ void Init_dbusglue(void)
 			INT2NUM(DBUS_MESSAGE_TYPE_ERROR));
 	rb_define_const(cDBUSMessage, "TYPE_SIGNAL",
 			INT2NUM(DBUS_MESSAGE_TYPE_SIGNAL));
-	rb_define_singleton_method(cDBUSConnection, "new",
+	rb_define_singleton_method(cDBUSMessage, "new",
 			rubydbus_message_new, 1);
+	rb_define_singleton_method(cDBUSMessage, "new_method_call",
+			rubydbus_message_new_method_call, 4);
+	rb_define_singleton_method(cDBUSMessage, "new_signal",
+			rubydbus_message_new_signal, 3);
+
+	rb_define_method(cDBUSMessage, "new_method_return",
+			rubydbus_message_new_method_return, 0);
+	rb_define_method(cDBUSMessage, "new_error",
+			rubydbus_message_new_error, 2);
 }
 
