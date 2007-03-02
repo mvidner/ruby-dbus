44a370c8687a81572e5dfdc42ed811b878ca08b7
paul <paul@a7605b1d-0065-4901-a58a-044c66c5aa93>
2007-03-02 13:02:52 +0000

    Removed dbusglue stuff from trunk.
    git-svn-id: https://svn.luon.net/svn/ruby-dbus/trunk@20 a7605b1d-0065-4901-a58a-044c66c5aa93

--- ext/dbus/extconf.rb
+++
@@ -1,9 +0,0 @@
-require 'mkmf'
-require 'pkg-config'
-
-unless PKGConfig.have_package("dbus-1", 1, 0, 0) or exit 1
-  puts "No D-Bus development files found!"
-  exit 1
-end
-
-create_makefile "dbusglue"
