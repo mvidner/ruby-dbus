873975c502ec09f691708d5192f99ef9b5d7a811
acornet <acornet@a7605b1d-0065-4901-a58a-044c66c5aa93>
2007-03-03 23:00:05 +0000

    Basic message creation.
    git-svn-id: https://svn.luon.net/svn/ruby-dbus/trunk@22 a7605b1d-0065-4901-a58a-044c66c5aa93

--- lib/dbus.rb
+++ lib/dbus.rb
@@ -12,8 +12,10 @@ module DBus
 
   if [0x01020304].pack("L").unpack("V")[0] == 0x01020304
     HOST_END = LIL_END
+    HOST_END_CHAR = ?l
   else
     HOST_END = BIG_END
+    HOST_END_CHAR = ?B
   end
 
   class InvalidPacketException < Exception
@@ -90,9 +92,9 @@ module DBus
       packet = nil
       case signature.sigtype
       when Type::BYTE
-        packet = get(1).unpack("C")
+        packet = get(1).unpack("C")[0]
       when Type::UINT32
-        packet = get(4).unpack(@uint32)
+        packet = get(4).unpack(@uint32)[0]
       when Type::ARRAY
         # checks please
         array_sz = get(4).unpack(@uint32)[0]
@@ -132,6 +134,140 @@ module DBus
     end
   end
 
+  class PacketMarshaller
+    attr_reader :packet
+    def initialize
+      @packet = ""
+    end
+
+    def align8
+      @packet = @packet.ljust(@packet.length + 7 & ~7, 0.chr)
+    end
+
+    def dump
+      p @packet
+    end
+
+    def setstring(str)
+      ret = ""
+      ret += [str.length].pack("L")
+      ret += str + "\0"
+      ret
+    end
+
+    def array
+      sizeidx = @packet.size
+      @packet += "ABCD"
+      align8
+      yield
+      sz = @packet.size - sizeidx - 4
+      raise InvalidPacketException if sz > 67108864
+      @packet[sizeidx...sizeidx + 4] = [sz].pack("L")
+    end
+
+    def struct
+      align8
+      yield
+    end
+
+    def append_string(s)
+      @packet += s + "\0"
+    end
+
+    def append(type, val)
+      case type
+      when Type::BYTE
+        @packet += val.chr
+      when Type::UINT32
+        @packet += [val].pack("L")
+      when Type::OBJECT_PATH
+        @packet += setstring(val)
+      when Type::STRING
+        @packet += setstring(val)
+      else
+        raise Exception, "not implemented"
+      end
+    end
+  end
+
+  class Message
+    INVALID = 0
+    METHOD_CALL = 1
+    METHOD_RETURN = 2
+    ERROR = 3
+    SIGNAL = 4
+
+    NO_REPLY_EXPECTED = 0x1
+    NO_AUTO_START = 0x2
+
+    attr_writer :message_type, :serial
+    attr_writer :path, :interface, :member, :error_name, :destination, :sender,
+      :signature
+
+    def initialize
+      @message_type = 0
+      @flags = 0
+      @protocol = 1
+      @body_length = 0
+      @serial = nil
+      @headers = Array.new
+    end
+
+    PATH = 1
+    INTERFACE = 2
+    MEMBER = 3
+    ERROR_NAME = 4
+    REPLY_SERIAL = 5
+    DESTINATION = 6
+    SENDER = 7
+    SIGNATURE = 8
+
+    def marshall
+      marshaller = PacketMarshaller.new
+      marshaller.append(Type::BYTE, HOST_END_CHAR)
+      marshaller.append(Type::BYTE, @message_type)
+      marshaller.append(Type::BYTE, @flags)
+      marshaller.append(Type::BYTE, @protocol)
+      marshaller.append(Type::UINT32, @body_length)
+      marshaller.append(Type::UINT32, @serial)
+      marshaller.array do
+        if @path
+          marshaller.struct do
+            marshaller.append(Type::BYTE, PATH)
+            marshaller.append(Type::BYTE, 1)
+            marshaller.append_string("o")
+            marshaller.append(Type::OBJECT_PATH, @path)
+          end
+        end
+        if @destination
+          marshaller.struct do
+            marshaller.append(Type::BYTE, DESTINATION)
+            marshaller.append(Type::BYTE, 1)
+            marshaller.append_string("s")
+            marshaller.append(Type::STRING, @destination)
+          end
+        end
+        if @interface
+          marshaller.struct do
+            marshaller.append(Type::BYTE, INTERFACE)
+            marshaller.append(Type::BYTE, 1)
+            marshaller.append_string("s")
+            marshaller.append(Type::STRING, @interface)
+          end
+        end
+        if @member
+          marshaller.struct do
+            marshaller.append(Type::BYTE, MEMBER)
+            marshaller.append(Type::BYTE, 1)
+            marshaller.append_string("s")
+            marshaller.append(Type::STRING, @member)
+          end
+        end
+      end
+      marshaller.packet
+    end
+  end
+
   class Connection
     def initialize(path)
       @path = path
@@ -147,15 +283,21 @@ module DBus
       init_connection
     end
 
-
     def writel(s)
       @socket.write("#{s}\r\n")
+    end
+
+    def send(buf)
+      @socket.write(buf)
+    end
+
+    def read(i)
+      @socket.read(i)
     end
 
     def readl
       @socket.readline.chomp
     end
-
 
     private
     def parse_session_string
@@ -180,5 +322,4 @@ module DBus
       writel("BEGIN")
     end
   end
-
 end # module DBus
---
+++ tests/conn3.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+require 'dbus'
+
+d = DBus::Connection.new(ENV["DBUS_SESSION_BUS_ADDRESS"])
+d.connect
+
+message = DBus::Message.new
+message.message_type = DBus::Message::METHOD_CALL
+message.serial = 1
+message.path = "/org/freedesktop/DBus"
+message.destination = "org.freedesktop.DBus"
+message.interface = "org.freedesktop.DBus"
+message.member = "Hello"
+
+d.send(message.marshall)
+d.send("\0\0") # ?? why ?
+s = d.read(90) # knowing 90 is cheating
+
+p s
+
