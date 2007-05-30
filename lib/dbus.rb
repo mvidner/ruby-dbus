318b587dbc785e014a242aad4f2d15ee90de4ef2
paul <paul@a7605b1d-0065-4901-a58a-044c66c5aa93>
2007-05-30 18:41:45 +0000

    Prepare the 0.2.0 release.
    git-svn-id: https://svn.luon.net/svn/ruby-dbus/trunk@121 a7605b1d-0065-4901-a58a-044c66c5aa93

--- lib/dbus.rb
+++ lib/dbus.rb
@@ -15,7 +15,7 @@ require 'dbus/bus.rb'
 require 'dbus/marshall'
 require 'dbus/message'
 require 'dbus/matchrule'
-require 'dbus/auth.rb'
+require 'dbus/auth'
 
 require 'socket'
 require 'thread'
@@ -32,7 +32,7 @@ module DBus
   # Byte signifying little endianness.
   LIL_END = ?l
 
-  # Byte signifying the host endianness
+  # Byte signifying the host's endianness.
   HOST_END = if [0x01020304].pack("L").unpack("V")[0] == 0x01020304
     LIL_END
   else
@@ -43,29 +43,38 @@ module DBus
   class InvalidPacketException < Exception
   end
 
-  # Exception raised when there is a problem with a type (may be unknown or mismatch).
+  # Exception raised when there is a problem with a type (may be unknown or
+  # mismatch).
   class TypeException < Exception
   end
 
-  # Exception raised when an unmarshalled buffer is truncated and incomplete
+  # Exception raised when an unmarshalled buffer is truncated and
+  # incomplete.
   class IncompleteBufferException < Exception
   end
 
+  # Exception raised when an interface is not implemented.
   class InterfaceNotImplemented < Exception
   end
 
+  # Exception raised when an method is not found in the interface.
   class MethodNotInInterface < Exception
   end
 
+  # Exception raised when a method has not been implemented (yet).
   class MethodNotImplemented < Exception
   end
 
+  # Exception raised when a method is invoked with invalid
+  # parameters (wrong number or type).
   class InvalidParameters < Exception
   end
 
+  # Exception raised when an invalid method name is used.
   class InvalidMethodName < Exception
   end
 
+  # Exception raised when invalid introspection data is parsed/used.
   class InvalidIntrospectionData < Exception
   end
 end # module DBus
