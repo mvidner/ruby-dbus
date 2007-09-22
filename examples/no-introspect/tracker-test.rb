a79421c0867975e72729ae477251bfe37a2d744f
acornet <acornet@a7605b1d-0065-4901-a58a-044c66c5aa93>
2007-09-22 21:31:44 +0000

    Add tracker example.
    git-svn-id: https://svn.luon.net/svn/ruby-dbus/trunk@132 a7605b1d-0065-4901-a58a-044c66c5aa93

---
+++ examples/no-introspect/tracker-test.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+#
+# Trivial network interface lister using NetworkManager.
+# NetworkManager does not support introspection, so the api is not that sexy.
+
+require 'dbus'
+
+bus = DBus::SessionBus.instance
+
+tracker_service = bus.service("org.freedesktop.Tracker")
+tracker_manager = tracker_service.object("/org/freedesktop/tracker")
+poi = DBus::ProxyObjectInterface.new(tracker_manager, "org.freedesktop.Tracker.Files")
+poi.define_method("GetMetadataForFilesInFolder", "in live_query_id:i, in uri:s, in fields:as, out values:aas")
+p poi.GetMetadataForFilesInFolder(-1, ENV['HOME'] + "/Desktop", ["File:Name", "File:Size"])
+
+
