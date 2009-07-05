a54ea4fb9e6077a7a63855f9cade26a7d58a3160
Martin Vidner <martin@vidner.net>
2009-07-05 19:38:36 +0200

    Added a test case for ticket 27 "a(uuuu)" (still failing).
    Test harness adjusted for that:
    test_server accepts make-like -k, enabled by test_all -a

---
+++ test/t3-ticket27.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+# Test passing a particular struct array through a variant
+# https://trac.luon.net/ruby-dbus/ticket/27
+require "dbus"
+session_bus = DBus::SessionBus.instance
+svc = session_bus.service("org.ruby.service")
+obj = svc.object("/org/ruby/MyInstance")
+obj.introspect                  # necessary
+obj.default_iface = "org.ruby.SampleInterface"
+# The bug is probably alignment related so whether it triggers
+# depends also on the combined length of service, interface,
+# and method names. Luckily here it works out.
+quadruple = ['a(uuuu)', []]     # a(uuu) works fine
+# The bus disconnects us because of malformed message,
+# code 12: DBUS_INVALID_TOO_MUCH_DATA
+obj.test_variant(quadruple)
