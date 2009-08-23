c3b2f362c150e335a269a800dda94bbc4c2a0635
Martin Vidner <martin@vidner.net>
2009-08-23 14:08:53 +0200

    Rubified test cases.
    By using test/unit

--- test/t2.rb
+++ test/t2.rb
@@ -1,9 +1,26 @@
 #!/usr/bin/env ruby
-# test passing an array through a variant
+require "test/unit"
 require "dbus"
-session_bus = DBus::SessionBus.instance
-svc = session_bus.service("org.ruby.service")
-obj = svc.object("/org/ruby/MyInstance")
-obj.introspect                  # necessary
-obj.default_iface = "org.ruby.SampleInterface"
-obj.test_variant(["as", ["coucou", "kuku"]])
+
+class ValueTest < Test::Unit::TestCase
+  def setup
+    session_bus = DBus::SessionBus.instance
+    svc = session_bus.service("org.ruby.service")
+    @obj = svc.object("/org/ruby/MyInstance")
+    @obj.introspect                  # necessary
+    @obj.default_iface = "org.ruby.SampleInterface"
+  end
+
+  def test_passing_an_array_through_a_variant
+    @obj.test_variant(["as", ["coucou", "kuku"]])
+  end
+
+  def test_marshalling_an_array_of_variants
+    # https://trac.luon.net/ruby-dbus/ticket/30
+    choices = []
+    choices << ['s', 'Plan A']
+    choices << ['s', 'Plan B']
+    @obj.default_iface = "org.ruby.Ticket30"
+    p @obj.Sybilla(choices)
+  end
+end
