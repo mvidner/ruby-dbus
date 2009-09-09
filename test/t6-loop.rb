adcfd0df2ecea5cea77dc95c64f11da37625c768
Martin Vidner <martin@vidner.net>
2009-09-09 15:52:27 +0200

    Added a test case for bnc#537401.
    https://bugzilla.novell.com/show_bug.cgi?id=537401

--- test/t6-loop.rb
+++ test/t6-loop.rb
@@ -18,17 +18,48 @@ class MainLoopTest < Test::Unit::TestCase
     @loop = DBus::Main.new
     @loop << @session_bus
   end
-#  def teardown ?
 
-  def test_loop_quit
+  def teardown
+    # workaround for nonexisting remove_match
+    @session_bus.instance_variable_set(:@signal_matchrules, [])
+  end
+
+  # Hack the library internals so that there is a delay between
+  # sending a DBus call and listening for its reply, so that
+  # the bus has a chance to join the server messages and a race is reproducible
+  def call_lazily
+    class << @session_bus
+      alias :wait_for_message_orig :wait_for_message
+      def wait_for_message_lazy
+        d "I am so lazy"
+        sleep 1    # Give the server+bus a chance to join the messages
+        wait_for_message_orig
+      end
+      alias :wait_for_message :wait_for_message_lazy
+    end
+
+    yield
+
+    # undo
+    class << @session_bus
+      remove_method :wait_for_message
+      remove_method :wait_for_message_lazy
+      alias :wait_for_message :wait_for_message_orig
+    end
+  end
+
+  def test_loop_quit(delay = 1)
     @obj.on_signal "LongTaskEnd" do
       d "Telling loop to quit"
       @loop.quit
     end
 
-    # The method will sleep the requested amount of seconds (in another thread)
-    # before signalling LongTaskEnd
-    @obj.LongTaskBegin 1
+    call_lazily do
+      # The method will sleep the requested amount of seconds
+      # (in another thread)  before signalling LongTaskEnd
+      @obj.LongTaskBegin delay
+    end
+
     # this thread will make the test fail if @loop.run does not return
     dynamite = Thread.new do
       d "Dynamite burning"
@@ -45,5 +76,10 @@ class MainLoopTest < Test::Unit::TestCase
     d "Defusing dynamite"
     # if we get here, defuse the bomb
     dynamite.exit
+  end
+
+  # https://bugzilla.novell.com/show_bug.cgi?id=537401
+  def test_loop_drained_socket
+    test_loop_quit 0
   end
 end
