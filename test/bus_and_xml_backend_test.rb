bb63c03a85f75e18b296def3b6b384e540932a28
Geoff Youngs <git@intersect-uk.co.uk>
2012-06-15 14:15:27 +0100

    Abstract XML parsing to support multiple backends; Add nokogiri support

---
+++ test/bus_and_xml_backend_test.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# Test the bus class
+require "test/unit"
+require 'rubygems'
+require 'nokogiri'
+require "dbus"
+
+class BusTest < Test::Unit::TestCase
+  def setup
+    @bus = DBus::ASessionBus.new
+  end
+
+  def test_introspection_reading_rexml
+    DBus::IntrospectXMLParser.backend = DBus::IntrospectXMLParser::REXMLParser
+    @svc = @bus.service("org.ruby.service")
+    obj = @svc.object("/org/ruby/MyInstance")
+    obj.default_iface = 'org.ruby.SampleInterface'
+    obj.introspect
+    assert_nothing_raised do
+      assert_equal 42, obj.the_answer[0], "should respond to :the_answer"
+    end
+    assert_nothing_raised do
+      assert_equal "oof", obj["org.ruby.AnotherInterface"].Reverse('foo')[0], "should work with multiple interfaces"
+    end
+  end
+
+  def test_introspection_reading_nokogiri
+    # peek inside the object to see if a cleanup step worked or not
+    DBus::IntrospectXMLParser.backend = DBus::IntrospectXMLParser::NokogiriParser
+    @svc = @bus.service("org.ruby.service")
+    obj = @svc.object("/org/ruby/MyInstance")
+    obj.default_iface = 'org.ruby.SampleInterface'
+    obj.introspect
+    assert_nothing_raised do
+      assert_equal 42, obj.the_answer[0], "should respond to :the_answer"
+    end
+    assert_nothing_raised do
+      assert_equal "oof", obj["org.ruby.AnotherInterface"].Reverse('foo')[0], "should work with multiple interfaces"
+    end
+  end
+
+end
