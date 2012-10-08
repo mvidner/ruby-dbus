ec8b795ae03a67964af3def1bc05c6aa6e1bc5d4
stoffie <stoffie@gmx.com>
2012-10-08 22:39:33 +0200

    Improved ProxyObjectInterface#define_method_from_descriptor

---
+++ test/introspection_test.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require "test/unit"
+require "dbus"
+
+class IntrospectionTest < Test::Unit::TestCase
+  def setup
+    session_bus = DBus::ASessionBus.new
+    svc = session_bus.service("org.ruby.service")
+    @obj = svc.object("/org/ruby/MyInstance")
+    @obj.introspect
+    @obj.default_iface = "org.ruby.SampleInterface"
+  end
+
+  def test_wrong_number_of_arguments
+    assert_raise(DBus::InvalidParameters) { @obj.test_variant "too","many","args" }
+    assert_raise(DBus::InvalidParameters) { @obj.test_variant } # not enough
+  end
+end
