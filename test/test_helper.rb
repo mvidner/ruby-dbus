7adc280a9f09badfecf2bc48e36eb0e0d0f7feb0
Martin Vidner <martin@vidner.net>
2013-04-30 00:31:15 +0200

    define SimpleCov.root so that service is not filtered out
    ... with 0% coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # Coverage: if available, but not in Travis
 if RUBY_VERSION >= "1.9" && ! ENV["CI"]
   require 'simplecov'
+  SimpleCov.root File.expand_path("../..", __FILE__)
   SimpleCov.start
 end
 $:.unshift File.expand_path("../../lib", __FILE__)
