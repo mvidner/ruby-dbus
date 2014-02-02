1264e588717dd0f3bb6d1d80fcce0a933e2d4069
Martin Vidner <martin@vidner.net>
2014-02-02 22:17:32 +0100

    prototyped setting up the bus and service inside the specs
    found a problem where GetConnectionUnixProcessID returns 0

--- test/bus_driver_spec.rb
+++ test/bus_driver_spec.rb
@@ -2,19 +2,105 @@
 require_relative "spec_helper"
 require "dbus"
 
-describe DBus::Service do
-  let(:bus) { DBus::ASessionBus.new }
+require "tempfile"
+require "timeout"
 
-  describe "#exists?" do
-    it "is true for an existing service" do
-      svc = bus.service("org.ruby.service")
-      svc.object("/").introspect # must activate the service first :-/
-      expect(svc.exists?).to be_true
+TOPDIR = File.expand_path("../..", __FILE__)
+
+def config_file_path
+  "#{TOPDIR}/test/tools/dbus-limited-session.conf"
+end
+
+def setup_private_bus
+  $temp_dir = Dir.mktmpdir
+  ENV["XDG_DATA_DIRS"] = $temp_dir
+
+  address_file = Tempfile.new("dbus-address")
+  pid_file     = Tempfile.new("dbus-pid")
+
+  cmd = "dbus-daemon --nofork --config-file=#{config_file_path} --print-address=3 3>#{address_file.path} --print-pid=4 4>#{pid_file.path} &"
+  system cmd
+
+  # wait until dbus-daemon writes the info
+  Timeout.timeout(10) do
+    until File.size?(address_file) and File.size?(pid_file) do
+      sleep 0.1
+    end
+  end
+
+  address = address_file.read.chomp
+  $pid = pid_file.read.chomp.to_i
+
+  ENV["DBUS_SESSION_BUS_ADDRESS"] = address
+end
+
+def teardown_private_bus
+  Process.kill("TERM", $pid)
+  FileUtils.rm_rf $temp_dir
+ end
+
+def setup_service_by_activation
+  name = "org.ruby.service"
+  exec = "#{TOPDIR}/test/service_newapi.rb"
+  $name = name
+  $exec = exec
+
+  service_dir = "#{$temp_dir}/dbus-1/services"
+  FileUtils.mkdir_p service_dir
+  # file name actually does not need to match the service name
+  File.open("#{service_dir}/#{name}.service", "w") do |f|
+    s = <<EOS
+[D-BUS Service]
+Name=#{name}
+Exec=#{exec}
+EOS
+    f.write(s)
+  end
+end
+
+def service_pid(name)
+  # use dbus_send, not the lib being tested
+  output = `dbus-send --print-reply=literal --dest=org.freedesktop.DBus / org.freedesktop.DBus.GetConnectionUnixProcessID string:#{name}`
+  output =~ /uint32 (\d+)/
+  $1.to_i
+end
+
+def teardown_service
+  kill_pid = service_pid($name)
+  if kill_pid != 0
+    Process.kill("TERM", kill_pid)
+  else
+    # for TCP connections; may be a bug, should get an error
+    # "Could not determine PID for '%s'"
+    puts "WTF"
+    system "pkill -f #{$exec}"
+  end
+end
+
+describe DBus::Service do
+  context "when a private bus is set up" do
+    before(:all) do
+      setup_private_bus
+      setup_service_by_activation
+    end
+    after(:all) do
+      teardown_service
+      teardown_private_bus
     end
 
-    it "is false for a nonexisting service" do
-      svc = bus.service("org.ruby.nosuchservice")
-      expect(svc.exists?).to be_false
+    let(:bus) { DBus::ASessionBus.new }
+
+    describe "#exists?" do
+      it "is true for an existing service" do
+        svc = bus.service("org.ruby.service")
+        svc.object("/").introspect # must activate the service first :-/
+        expect(svc.exists?).to be_true
+      end
+
+      it "is false for a nonexisting service" do
+        svc = bus.service("org.ruby.nosuchservice")
+        expect(svc.exists?).to be_false
+      end
     end
   end
 end
