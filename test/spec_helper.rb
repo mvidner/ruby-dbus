2d34b2920eef0ae78d9548ffe88e7565c675c3c8
Martin Vidner <martin@vidner.net>
2014-02-03 22:15:00 +0100

    restrict specs to "expect" syntax

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,2 +1,9 @@
 require_relative "test_helper"
 SimpleCov.command_name "Specs" if Object.const_defined? "SimpleCov"
+
+# http://betterspecs.org/#expect
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
