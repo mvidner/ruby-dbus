ca4c78211f93899a2b6c2301c163d5df25f3dcdd
Martin Vidner <martin@vidner.net>
2014-02-05 15:51:16 +0100

    use coveralls for on-line code coverage reporting at Travis CI
    thanks to @lslezak for setting the example in
    https://github.com/yast/rubygem-scc_api/pull/9

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,22 @@
 if ENV["COVERAGE"]
   coverage = ENV["COVERAGE"] == "true"
 else
-  # heuristics: enable for interactive builds but not in Travis or OBS
-  coverage = !!ENV["DISPLAY"]
+  # heuristics: enable for interactive builds (but not in OBS) or in Travis
+  coverage = !!ENV["DISPLAY"] || ENV["TRAVIS"]
 end
 
-if coverage && RUBY_VERSION >= "1.9" # SimpleCov does not work with 1.8
-  require 'simplecov'
+if coverage
+  require "simplecov"
   SimpleCov.root File.expand_path("../..", __FILE__)
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
   SimpleCov.start
 end
 
