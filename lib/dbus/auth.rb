3ad154b072993f3ccba94e1eb2b18b3bc3fc1f1e
Christian Taedcke <hacking@taedcke.com>
2014-02-09 13:52:51 +0100

    Add NEGOTIATE_UNIX_FD during authentication.

--- lib/dbus/auth.rb
+++ lib/dbus/auth.rb
@@ -230,8 +230,8 @@ module DBus
           send("CANCEL")
           @state = :WaitingForReject
         when "OK"
-          send("BEGIN")
-          @state = :Authenticated
+          send("NEGOTIATE_UNIX_FD")
+          @state = :WaitingForUnixFd
         else
           send("ERROR")
           @state = :WaitingForData
@@ -240,8 +240,8 @@ module DBus
         DBus.logger.debug ":WaitingForOk msg: #{msg[0].inspect}"
         case msg[0]
         when "OK"
-          send("BEGIN")
-          @state = :Authenticated
+          send("NEGOTIATE_UNIX_FD")
+          @state = :WaitingForUnixFd
         when "REJECT"
           next_authenticator
           @state = :WaitingForData
@@ -261,6 +261,19 @@ module DBus
         else
           @socket.close
           return false
+        end
+      when :WaitingForUnixFd
+         DBus.logger.debug ":WaitingForUnixFd msg: #{msg[0].inspect}"
+        case msg[0]
+        when "AGREE_UNIX_FD"
+          send("BEGIN")
+          @state = :Authenticated
+        when "ERROR"
+          send("BEGIN")
+          @state = :Authenticated
+        else
+          send("BEGIN")
+          @state = :Authenticated
         end
       end
       return true
