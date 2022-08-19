743b82e9b1ba195902defd32ff6ff11521735547
Martin Vidner <martin@vidner.net>
2022-08-19 12:55:45 +0200

    Test Node#descendant_objects

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -20,4 +20,46 @@ describe DBus::Node do
       expect(parent.inspect).to match(/<DBus::Node [0-9a-f]+ {child0 => {},child1 => {},child2 => {}}>/)
     end
   end
+
+  describe "#descendant_objects" do
+    let(:manager_path) { "/org/example/FooManager" }
+    let(:child_paths) do
+      [
+        # note that "/org/example/FooManager/good"
+        # is a path under a managed object but there is no object there
+        "/org/example/FooManager/good/1",
+        "/org/example/FooManager/good/2",
+        "/org/example/FooManager/good/3",
+        "/org/example/FooManager/bad/1",
+        "/org/example/FooManager/bad/2"
+      ]
+    end
+
+    let(:non_child_paths) do
+      [
+        "/org/example/BarManager/good/1",
+        "/org/example/BarManager/good/2"
+      ]
+    end
+
+    context "on the bus" do
+      let(:bus) { DBus::ASessionBus.new }
+      let(:service) { bus.request_service("org.ruby.service") }
+
+      before do
+        service.export(DBus::Object.new(manager_path))
+        non_child_paths.each do |p|
+          service.export(DBus::Object.new(p))
+        end
+      end
+
+      it "returns just the descendants of the specified objects" do
+        child_exported_objects = child_paths.map { |p| DBus::Object.new(p) }
+        child_exported_objects.each { |obj| service.export(obj) }
+
+        node = service.get_node(manager_path, create: false)
+        expect(node.descendant_objects).to eq child_exported_objects
+      end
+    end
+  end
 end
