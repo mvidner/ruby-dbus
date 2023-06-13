0a9489fd76e553fc8cd0f8447bc88b31c897b760
Martin Vidner <martin@vidner.net>
2023-06-13 10:03:55 +0200

    Removed DBus::Connection::DBUSXMLINTRO, moved its contents to a .xml file
    It only contains the main interface, org.freedesktop.DBus, not others:
    Properties, Introspectable, Monitoring, Debug.Stats, Peer.
    
    Need to update it and figure out a better API than the existing `bus.proxy`.
    
    Use:
    
    ```
    busctl introspect --xml-interface org.freedesktop.DBus /org/freedesktop/DBus
    ```

--- lib/dbus/bus.rb
+++ lib/dbus/bus.rb
@@ -90,106 +90,6 @@ module DBus
     REQUEST_NAME_REPLY_EXISTS = 0x3
     REQUEST_NAME_REPLY_ALREADY_OWNER = 0x4
 
-    DBUSXMLINTRO = '<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
-"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
-<node>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg direction="out" type="s"/>
-    </method>
-  </interface>
-  <interface name="org.freedesktop.DBus">
-    <method name="Hello">
-      <arg direction="out" type="s"/>
-    </method>
-    <method name="RequestName">
-      <arg direction="in" type="s"/>
-      <arg direction="in" type="u"/>
-      <arg direction="out" type="u"/>
-    </method>
-    <method name="ReleaseName">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="u"/>
-    </method>
-    <method name="StartServiceByName">
-      <arg direction="in" type="s"/>
-      <arg direction="in" type="u"/>
-      <arg direction="out" type="u"/>
-    </method>
-    <method name="UpdateActivationEnvironment">
-      <arg direction="in" type="a{ss}"/>
-    </method>
-    <method name="NameHasOwner">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="b"/>
-    </method>
-    <method name="ListNames">
-      <arg direction="out" type="as"/>
-    </method>
-    <method name="ListActivatableNames">
-      <arg direction="out" type="as"/>
-    </method>
-    <method name="AddMatch">
-      <arg direction="in" type="s"/>
-    </method>
-    <method name="RemoveMatch">
-      <arg direction="in" type="s"/>
-    </method>
-    <method name="GetNameOwner">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="s"/>
-    </method>
-    <method name="ListQueuedOwners">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="as"/>
-    </method>
-    <method name="GetConnectionUnixUser">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="u"/>
-    </method>
-    <method name="GetConnectionUnixProcessID">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="u"/>
-    </method>
-    <method name="GetAdtAuditSessionData">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="ay"/>
-    </method>
-    <method name="GetConnectionSELinuxSecurityContext">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="ay"/>
-    </method>
-    <method name="ReloadConfig">
-    </method>
-    <method name="GetId">
-      <arg direction="out" type="s"/>
-    </method>
-    <method name="GetConnectionCredentials">
-      <arg direction="in" type="s"/>
-      <arg direction="out" type="a{sv}"/>
-    </method>
-    <property name="Features" type="as" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="const"/>
-    </property>
-    <property name="Interfaces" type="as" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="const"/>
-    </property>
-    <signal name="NameOwnerChanged">
-      <arg type="s"/>
-      <arg type="s"/>
-      <arg type="s"/>
-    </signal>
-    <signal name="NameLost">
-      <arg type="s"/>
-    </signal>
-    <signal name="NameAcquired">
-      <arg type="s"/>
-    </signal>
-  </interface>
-</node>
-'
-    # This apostroph is for syntax highlighting editors confused by above xml: "
-
     # @api private
     # Send a _message_.
     # If _reply_handler_ is not given, wait for the reply
@@ -301,10 +201,13 @@ module DBus
     # Returns the object.
     def proxy
       if @proxy.nil?
+        xml_filename = File.expand_path("org.freedesktop.DBus.xml", __dir__)
+        xml = File.read(xml_filename)
+
         path = "/org/freedesktop/DBus"
         dest = "org.freedesktop.DBus"
         pof = DBus::ProxyObjectFactory.new(
-          DBUSXMLINTRO, self, dest, path,
+          xml, self, dest, path,
           api: ApiOptions::A0
         )
         @proxy = pof.build["org.freedesktop.DBus"]
